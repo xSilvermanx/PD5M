4820e9fa595e9b842cc71819436d878960e61a28
xSilvermanx <62330001+xSilvermanx@users.noreply.github.com>
2020-03-21 20:58:38 +0100

    Add files via upload

--- HUD/tp_sv.lua
+++ HUD/tp_sv.lua
@@ -1,8 +1,10 @@
 -- Handler to utilize "/tp" in tp_cl.lua
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/tp" then
-		CancelEvent()
-		TriggerClientEvent('pd5m:hud:opentpmenu', s)
-	end
-end)
+if ActivateTPfunction then
+	AddEventHandler('chatMessage', function(s, n, m)
+		local message = string.lower(m)
+		if message == "/tp" then
+			CancelEvent()
+			TriggerClientEvent('pd5m:hud:opentpmenu', s)
+		end
+	end)
+end
