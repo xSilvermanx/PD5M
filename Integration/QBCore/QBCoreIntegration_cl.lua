5af436856f32da1ac9b9c88415bbc8963c4f7308
xSilvermanx <93seppo93@gmx.de>
2023-10-22 14:07:39 +0200

    QBCore Fix

--- Integration/QBCore/QBCoreIntegration_cl.lua
+++ Integration/QBCore/QBCoreIntegration_cl.lua
@@ -1,9 +1,16 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+PlayerData = QBCore.Functions.GetPlayerData()
+
 Citizen.CreateThread(function()
-	local PlayerData = QBCore.Functions.GetPlayerData()
-	if PlayerData.job.name == 'ambulance' or PlayerData.job.name == 'fire' then
-      PlayerData.job.name = 'police'
-    end
-	Wait(5000)
+	while true do
+		PlayerData = QBCore.Functions.GetPlayerData()
+		if PlayerData and PlayerData.job and PlayerData.job.name then
+			if PlayerData.job.name == 'ambulance' or PlayerData.job.name == 'fire' then
+				PlayerData.job.name = 'police'
+			end
+		end
+		Wait(5000)
+	end
 end)
+	
